ea4cec495f5d38d795a1dccfc7e5a547b4c6ba0d
HamzDev <0963707@hr.nl>
2018-12-18 11:19:34 +0100

    Code clean up

--- Client/src/com/design/main/MainDisplayController.java
+++ Client/src/com/design/main/MainDisplayController.java
@@ -12,19 +12,45 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * @author Furkan, Abid, Zakaria, Usman, Hamza
+ * @version 1.5
+ */
 public class MainDisplayController {
 
-    public ImageView secondDiceImage;
-    public ImageView firstDiceImage;
-    public ImageView spinner;
+    /**
+     * Images views objects for the 2 dices and spinner
+     */
+    public ImageView secondDiceImage, firstDiceImage, spinner;
 
+    /**
+     * Text area for the assignment
+     */
     public TextArea cardTypeAssignment;
+
+    /**
+     * Text for spinner display
+     */
     public Label spinnerLabel;
 
+    /**
+     * Random number generator
+     */
     private Random random = new Random();
 
+    /**
+     * Anti(spam) boolean
+     */
     private boolean running = false;
 
+    /**
+     * Card data
+     */
+    private CardData current;
+
+    /**
+     * Executes on dice click
+     */
     public void onDiceClick() {
         if(running) {
             return;
@@ -54,10 +80,17 @@ public class MainDisplayController {
         timer.schedule(task, 50, 50);
     }
 
+    /**
+     * Fetches random data from dice data (enum)
+     * @return
+     */
     private DiceRollData getDiceData() {
         return DiceRollData.values()[random.nextInt(DiceRollData.values().length)];
     }
 
+    /**
+     * Executes on spin click
+     */
     public void onSpinnerClick(){
         if(running) {
             return;
@@ -81,8 +114,9 @@ public class MainDisplayController {
         timer.schedule(task, 20, 20);
     }
 
-    private CardData current;
-
+    /**
+     * Executes on card click
+     */
     public void onCardClick() {
         if(running) {
             return;
@@ -115,6 +149,10 @@ public class MainDisplayController {
         timer.schedule(task, 20, 20);
     }
 
+    /**
+     * Fetches random card (data)
+     * @return
+     */
     private CardData getCardData() {
         return CardData.values()[random.nextInt(CardData.values().length)];
     }
